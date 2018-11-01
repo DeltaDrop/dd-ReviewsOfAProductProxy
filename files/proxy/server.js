6f2e0b872d95be8ecd63d0fdd401941b4bc598b7
Kenni Silverio <ksilverio31@gmail.com>
2018-11-01 19:16:08 -0400

    adding all changes made to master, proxy server is up and running except for the css on product info

---
+++ files/proxy/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3004;
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const circularJson = require('circular-json');
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json())
+
+// app.get('/item/:id', (req, res)=> {
+//   axios.get(`http://localhost:3001${req.url}`)
+//   .then(data => {
+//     res.status(200).send(circularJson.stringify(data))
+//   })
+//   .catch(err => {console.log(err, ' err in get productinfo item in proxy')})
+// })
+
+// app.get('/categories/:id', (req, res)=> {
+//   axios.get(`http://localhost:3001${req.url}`)
+//   .then(data => {
+//     res.status(200).send(circularJson.stringify(data))
+//   })
+//   .catch(err => {console.log(err, ' err in get productinfo category in proxy')})
+// })
+
+// app.get('/buy/:productname/reviews', (req, res)=> {
+//   axios.get(`http://localhost:3002${req.url}`)
+//   .then(data => {
+//     res.status(200).send(circularJson.stringify(data))
+//   })
+//   .catch(err => {console.log(err, ' err in get reviews in proxy')})
+// })
+
+// app.get('/buy/:prod_name/overview', (req, res)=> {
+//   axios.get(`http://localhost:3003${req.url}`)
+//   .then(data => {
+//     res.status(200).send(circularJson.stringify(data))
+//   })
+//   .catch(err => {console.log(err, ' err in get overview in proxy')})
+// })
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
